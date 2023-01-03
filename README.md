e875e34d1bf4475e702017d8ce549c25c62f84f3
Mykola Raryk <mykola.raryk@west.com>
2023-01-03 12:32:41 +0200

    script has been added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # telegram_bot_uptime
 Check uptime of server and send status to telegram
+
+Telegram bot sends message to you when server is up and running.
+
+python = 3.9.7
+
+Library - https://github.com/luckydonald/pytgbot#releases
---
+++ main.py
@@ -0,0 +1,72 @@
+from pytgbot import Bot
+import subprocess
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+API_KEY='123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'  # change this to the token you get from @BotFather
+CHAT_ID='11111111'  # can be a @username or a id, change this to your own @username or id for example.
+
+bot = Bot(API_KEY)
+
+# get uptime of server
+def uptime():
+    upTime = subprocess.check_output(['uptime']).decode('UTF-8')
+    return upTime
+
+# send message on start bot
+def start_bot():
+    bot.send_message(CHAT_ID, "Example Text!")
+
+# help messages
+def help():
+    msg = '''
+Server Uptime Bot
+---------
+Uptime  → /uptime
+---------
+    '''
+    return msg
+
+# Check if we have connection to internet
+def ping_check():
+    for ping in range(1,100):
+        address = "8.8.8.8"
+        res = subprocess.call(['ping', '-c', '5', address])
+        if res == 0:
+            print ("ping to", address, "OK")
+            return True
+        else:
+            print ("ping to", address, "failed!")
+    return False
+
+
+# reply to message
+def request_bot():
+    last_update_id = -1
+    while True:
+        # loop forever.
+        for update in bot.get_updates(limit=100, offset=last_update_id+1, poll_timeout=30):
+            last_update_id = update.update_id
+            print("got message: {msg}".format(msg=update))
+            if not "message" in update:
+                continue
+            message = update.message
+            if not "text" in message:
+                continue
+            logger.debug("got text: {msg}".format(msg=message.text.encode("utf-8")))
+            if not message.text == "/uptime":
+                bot.send_message(CHAT_ID, help())
+                continue
+            #origin = message.chat.id if "chat" in message else message.from_peer.id
+            bot.send_message (CHAT_ID, uptime())
+
+def main():
+    if ping_check():
+        start_bot()
+        request_bot()
+
+if __name__ == '__main__':
+    main()
+
